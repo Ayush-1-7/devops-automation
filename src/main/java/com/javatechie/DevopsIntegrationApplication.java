5afa35798ccda342215d46642b010b373a96db54
Ayush-1-7 <ayush17v@gmail.com>
2024-04-19 11:08:17 +0530

    Welcome from Ayush

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "welcome to javatechie";
+		return " WELCOME FROM AYUSH ";
 	}
 
 	public static void main(String[] args) {
